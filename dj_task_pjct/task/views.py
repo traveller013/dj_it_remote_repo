6ae9881ed8cfc962b73ff70b980b10740c9bb1e5
traveller013 <htes013@gmail.com>
2022-05-15 15:31:33 -0700

    added temples, models
    working repo with starter template and models

--- dj_task_pjct/task/views.py
+++ dj_task_pjct/task/views.py
@@ -1,6 +1,14 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import *
+
 # Create your views here.
 def index(request):
-    return HttpResponse('hello world')
+    tasks = Task.objects.all()
+    context = {
+        'tasks': tasks,
+    }
+    return render(request , 'task/list.html',context)
 
