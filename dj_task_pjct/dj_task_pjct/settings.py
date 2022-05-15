4bfc8e7294940d373ff7fdd31f7bd2c381378931
traveller013 <htes013@gmail.com>
2022-05-15 15:12:49 -0700

    pjct w/ new basic url routing
    project with basic url routing

--- dj_task_pjct/dj_task_pjct/settings.py
+++ dj_task_pjct/dj_task_pjct/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #don't forgot commas
+    'task',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
