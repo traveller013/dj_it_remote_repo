9e213baab172b337b606b670ec081c9f9ff229a5
traveller013 <htes013@gmail.com>
2022-05-23 17:33:21 -0700

    pre-user function
    but it has CRUD functions

--- todo/base/urls.py
+++ todo/base/urls.py
@@ -1,6 +1,6 @@
 from importlib.resources import path
 from django.urls import path
-from .views import TaskCreate, TaskDetail, TaskList
+from .views import TaskCreate, TaskDelete, TaskDetail, TaskList, TaskUpdate
 from .models import Task
 urlpatterns = [
     #need commas after every entry
@@ -8,4 +8,6 @@ urlpatterns = [
     path('',TaskList.as_view(),name = 'tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name = 'tasks' ),
     path('task-create/', TaskCreate.as_view(), name = 'task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name = 'task-update'),
+    path('task-delete/<int:pk>/', TaskDelete.as_view() , name="task-delete")
 ]
--- todo/base/views.py
+++ todo/base/views.py
@@ -2,7 +2,7 @@ from asyncio import tasks
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView , UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -22,3 +22,13 @@ class TaskCreate(CreateView):
     model = Task
     fields = '__all__'
     success_url= reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
