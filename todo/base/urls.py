178608cd4f3381c93307c74996bfc610ece0501a
traveller013 <htes013@gmail.com>
2022-05-25 13:10:01 -0700

    login added
    functionally done todo app

--- todo/base/urls.py
+++ todo/base/urls.py
@@ -1,13 +1,23 @@
 from importlib.resources import path
 from django.urls import path
-from .views import TaskCreate, TaskDelete, TaskDetail, TaskList, TaskUpdate
-from .models import Task
-urlpatterns = [
-    #need commas after every entry
-    #path('' , views.index, name = "list"),
+
+#from .models import task
+from .views import CustomizedLoginView, TaskCreate, TaskDelete, TaskDetail, TaskList, TaskUpdate,RegisterPage
+    #enables logout without another template
+from django.contrib.auth.views import LogoutView
+
+
+
+urlpatterns = [#need commas after every entry
+    
+    path('login' , CustomizedLoginView.as_view(),  name = "login"),
+    path('logout/', LogoutView.as_view(next_page= 'login'), name = 'logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
+   
     path('',TaskList.as_view(),name = 'tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name = 'tasks' ),
     path('task-create/', TaskCreate.as_view(), name = 'task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name = 'task-update'),
-    path('task-delete/<int:pk>/', TaskDelete.as_view() , name="task-delete")
+    path('task-delete/<int:pk>/', TaskDelete.as_view() , name="task-delete"),
+    
 ]
--- todo/base/views.py
+++ todo/base/views.py
@@ -1,34 +1,78 @@
-from asyncio import tasks
-from django.shortcuts import render
+
+from django.shortcuts import render , redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView , UpdateView, DeleteView
+from django.views.generic.edit import CreateView , UpdateView, DeleteView , FormView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
 from .models import Task
 
+#user restrictions - add to views so they can't go straight there with an address
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+
 # Create your views here.
-class TaskList(ListView):
+
+class CustomizedLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user= True
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+class RegisterPage(FormView):
+    template_name= 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user= True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args,**kwargs)
+
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = "tasks"
-
-class TaskDetail(DetailView):
+        #user restrict-  only see their own stuff
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete = False).count()
+        
+        return context
+class TaskDetail(LoginRequiredMixin,DetailView):
     model = Task
         #sets class findable 'nickname'
     context_object_name = 'task'
         #sets pathing from 'template' '_' 'name' to "new"
     template_name = 'base/task.html'
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin,CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title','description','complete']
     success_url= reverse_lazy('tasks')
 
-class TaskUpdate(UpdateView):
+    def form_valid(self, form) :
+            #user restric- only creates to
+        form.instance.user = self.request.user
+        return super(TaskCreate,self).form_valid(form)
+
+class TaskUpdate(LoginRequiredMixin,UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['description','title', 'complete']
+        
     success_url = reverse_lazy('tasks')
 
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin,DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
+
